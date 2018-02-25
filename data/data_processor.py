0e121daa96d0a0ae24947e18c569b1dda9b962db
willbe <rlakim5521@naver.com>
2018-02-25 17:10:27 -0600

    1. Add dummy data_processor functions.

---
+++ data/data_processor.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Jaekyoung Kim
+:Date: 2018. 2. 25.
+"""
+import pandas as pd
+import numpy as np
+
+from data.data_reader import get_champs, get_participants
+
+
+def get_same_team_matches():
+    """
+
+    :return same_team_matches: (numpy.array[int]) 690 * 690 matrix with number of matches between champions
+        in same team.
+    """
+    pass
+
+
+def get_same_team_wins():
+    """
+
+    :return same_team_wins: (numpy.array[int]) 690 * 690 matrix with number of matches between champions
+        in same team.
+    """
+    pass
+
+
+def get_same_team_loses():
+    """
+
+    :return same_team_loses: (numpy.array[int]) 690 * 690 matrix with number of matches between champions
+        in same team.
+    """
+    pass
+
+
+def get_other_team_matches():
+    """
+
+    :return other_team_matches: (numpy.array[int]) 690 * 690 matrix with number of matches between champions
+        in other teams.
+    """
+    pass
+
+
+def get_other_team_wins():
+    """
+
+    :return other_team_wins: (numpy.array[int]) 690 * 690 matrix with number of matches between champions
+        in other teams.
+    """
+    pass
+
+
+def get_other_team_loses():
+    """
+
+    :return other_team_loses: (numpy.array[int]) 690 * 690 matrix with number of matches between champions
+        in other teams.
+    """
+    pass
