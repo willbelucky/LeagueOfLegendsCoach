1d3b2726c5d744111ace39a3ae9451ba51f903d8
willbe <rlakim5521@naver.com>
2018-02-28 16:55:49 -0600

    1. Design distance_calculator.

---
+++ stats/distance_calculator.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: Jaekyoung Kim
+:Date: 2018. 2. 28.
+"""
+import pandas as pd
+import numpy as np
+
+
+def calculate_collaborative_distance():
+    """
+
+    :return:
+    """
+    # Check /stats/distance.csv.
+
+    # If /stats/distance.csv is exists, return /stats/distance.csv as a 680 * 680 numpy.ndarray.
+
+    # If there is no /stats/distance.csv, do below codes.
+    # Get matrix of matches, wins ans loses.
+
+    # Calculate the distance between collaborative players.
+
+    # Save the result to /stats/distance.csv.
+
+    # Return /stats/distance.csv as a 680 * 680 numpy.ndarray.
+
+
+def calculate_competitive_distance():
+    """
+
+    :return:
+    """
+    # Check /stats/distance.csv.
+
+    # If /stats/distance.csv is exists, return /stats/distance.csv as a 680 * 680 numpy.ndarray.
+
+    # If there is no /stats/distance.csv, do below codes.
+    # Get matrix of matches, wins ans loses.
+
+    # Calculate the distance between competitive players.
+
+    # Save the result to /stats/distance.csv.
+
+    # Return /stats/distance.csv as a 680 * 680 numpy.ndarray.
